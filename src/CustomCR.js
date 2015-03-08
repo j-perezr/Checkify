e401d7049acfe06c6e491a8cdcba0954579853b4
Javier Pérez Ruiz <jperezruiz@gmail.com>
2015-03-08 17:22:50 +0100

    Implemented change event handled Implemented _setChecked to change component and input checked state Implemented check, uncheck and toggle functions

--- src/CustomCR.js
+++ src/CustomCR.js
@@ -4,24 +4,24 @@ var CustomCR = (function () {
         this._init(params);
     };
     CustomCR.prototype = {
-        CLASS_CHECKBOX: "cr-checkbox",
-        CLASS_RADIO: "cr-radio",
-        CLASS_CHECKED: "cr-checked",
-        CLASS_FALSELY_INPUT: "cr-input",
-        CLASS_INPUT: "cr-nativeInput",
-        CLASS_LABEL: "cr-label",
-        CLASS_WRAPPER: "cr-container",
-        CLASS_FOCUS: "cr-focus",
-        CLASS_ACTIVE: "cr-active",
-        CLASS_DISABLED: "cr-disabled",
-        CLASS_HOVER:"cr-hover",
-        EVENT_NAMESPACE: ".customcr",
-        _DEFAULTS: {
+        CLASS_CHECKBOX        : "cr-checkbox",
+        CLASS_RADIO           : "cr-radio",
+        CLASS_CHECKED         : "cr-checked",
+        CLASS_FALSELY_INPUT   : "cr-input",
+        CLASS_INPUT           : "cr-nativeInput",
+        CLASS_LABEL           : "cr-label",
+        CLASS_WRAPPER         : "cr-container",
+        CLASS_FOCUS           : "cr-focus",
+        CLASS_ACTIVE          : "cr-active",
+        CLASS_DISABLED        : "cr-disabled",
+        CLASS_HOVER           : "cr-hover",
+        EVENT_CHANGE          : "crchange",
+        EVENT_NAMESPACE       : ".customcr",
+        _DEFAULTS             : {
             wrap: false,
-            active: false,
             disabled: false
         },
-        _init: function (params) {
+        _init                 : function (params) {
             //get input
             var attributes,
                 parent,
@@ -43,7 +43,7 @@ var CustomCR = (function () {
             falselyInput = this._createFalseInput(type);
             //insert falsely input and append native input into falselyInput
             falselyInput.insertAfter(masterNode)
-                                   .append(masterNode);
+                .append(masterNode);
             if (attributes.wrap === true) {
                 wrapper = this._createWrapper();
                 wrapper.insertAfter(falselyInput);
@@ -60,65 +60,43 @@ var CustomCR = (function () {
             //create falsely input
             //data on node
         },
-        _assignEvents: function (labelExists) {
+        _assignEvents         : function (labelExists) {
             var falselyInput = this.attributes.falselyInput,
                 masterNode = this.attributes.masterNode;
             //masterNode.on("click"+this.EVENT_NAMESPACE,this._preventDefault);
             falselyInput.on("click" + this.EVENT_NAMESPACE +
-                            " touchstart" +this.EVENT_NAMESPACE+
-                            " touchend" + this.EVENT_NAMESPACE+
-                            " mousedown"+ this.EVENT_NAMESPACE+
-                            " mouseup" + this.EVENT_NAMESPACE+
-                            " mouseover"+ this.EVENT_NAMESPACE+
-                            " mouseout"+ this.EVENT_NAMESPACE,{instance:this},this._onEventTriggered);
-            masterNode.on("change"+ this.EVENT_NAMESPACE+
-                          " focus" +this.EVENT_NAMESPACE +
-                          " blur" +this.EVENT_NAMESPACE+
-                          " keydown" +this.EVENT_NAMESPACE+
-                          " keyup"+ this.EVENT_NAMESPACE,{instance:this},this._onEventTriggered);
-            //click event, change state
-            //falselyInput.on("click" + this.EVENT_NAMESPACE + " touchleave" + this.EVENT_NAMESPACE, {instance: this}, this._onFalselyInputOperated);
-            //mousedown event, change active state
-            //falselyInput.on("mousedown touchstart" + this.EVENT_NAMESPACE, {instance: this}, this._onKeyDown);
-            //mouseup event, change active state
-            //falselyInput.on("mouseup touchend" + this.EVENT_NAMESPACE, {instance: this}, this._onKeyUp);
-            //hover
-            //falselyInput.on("mouseover" + this.EVENT_NAMESPACE, {instance: this}, this._onMouseOver);
-            //mouseout
-            //falselyInput.on("mouseout" + this.EVENT_NAMESPACE, {instance: this}, this._onMouseOut);
-            //native change event, change state
-            //masterNode.on("change" + this.EVENT_NAMESPACE, {instance: this}, this._onInputChange);
-            //focus event, change focus state
-            //masterNode.on("focus" + this.EVENT_NAMESPACE, {instance: this}, this._onFocus);
-            //blur event, change focus state
-            //masterNode.on("blur" + this.EVENT_NAMESPACE, {instance: this}, this._onBlur);
-            //keydown event on native input, change active state
-            //masterNode.on("keydown touchstart" + this.EVENT_NAMESPACE, {instance: this}, this._onKeyDown);
-            //keydup event on native input, change active state
-            //masterNode.on("keyup touchend" + this.EVENT_NAMESPACE, {instance: this}, this._onKeyUp);
+                            " touchstart" + this.EVENT_NAMESPACE +
+                            " touchend" + this.EVENT_NAMESPACE +
+                            " mousedown" + this.EVENT_NAMESPACE +
+                            " mouseup" + this.EVENT_NAMESPACE +
+                            " mouseover" + this.EVENT_NAMESPACE +
+                            " mouseout" + this.EVENT_NAMESPACE, {instance: this}, this._onEventTriggered);
+            masterNode.on("click" + this.EVENT_NAMESPACE +
+                          " change" + this.EVENT_NAMESPACE +
+                          " focus" + this.EVENT_NAMESPACE +
+                          " blur" + this.EVENT_NAMESPACE +
+                          " keydown" + this.EVENT_NAMESPACE +
+                          " keyup" + this.EVENT_NAMESPACE, {instance: this}, this._onEventTriggered);
         },
-        check: function () {
+        check                 : function () {
+            this._setChecked(true);
+        },
+        uncheck               : function () {
+            this._setChecked(false);
+        },
+        toggle                : function () {
+            this._setChecked(!this.attributes.masterNode.prop("checked"));
+        },
+        disable               : function (isDisabled) {
 
         },
-        uncheck: function () {
+        destroy               : function () {
 
         },
-        toggle: function () {
-
-        },
-        disable: function (isDisabled) {
-
-        },
-        destroy: function () {
-
-        },
-        _radioChange: function (checked) {
-
-        },
-        _createWrapper: function () {
+        _createWrapper        : function () {
             return $('<div class=' + this.CLASS_WRAPPER + '></div>');
         },
-        _createFalseInput: function (type) {
+        _createFalseInput     : function (type) {
             return $("<div class='" + this.CLASS_FALSELY_INPUT + " " + (type === "checkbox" ? this.CLASS_CHECKBOX : this.CLASS_RADIO) + "'></div>");
         },
         _extractDataAttributes: function (masterNode) {
@@ -127,30 +105,29 @@ var CustomCR = (function () {
             params.disabled = masterNode.attr("disabled");
             return params;
         },
-        _setChecked: function (checked) {
-            var attributes = this.attributes;
+        _setChecked           : function (checked) {
+            var attributes = this.attributes,
+                masterNode = attributes.masterNode;
             if (attributes.disabled === false || attributes.masterNode.attr("disabled") === undefined) {
                 attributes.checked = checked;
-                attributes.masterNode.prop("checked", checked);
-                attributes.masterNode.attr("checked", checked === true ? "checked": "");
-                if (checked === true) {
-                    attributes.falselyInput.addClass(this.CLASS_CHECKED);
-                    attributes.label.addClass(this.CLASS_CHECKED);
-                    if(attributes.wrapper) {
-                        attributes.wrapper.addClass(this.CLASS_CHECKED);
+                this._updateState(checked, this.CLASS_CHECKED);
+                if (masterNode.prop("checked") !== checked) {
+                    masterNode.prop("checked", checked);
+                    if (checked) {
+                        masterNode.attr("checked", "checked");
+                    } else {
+                        masterNode.removeAttr("checked");
                     }
-                } else {
-                    attributes.falselyInput.removeClass(this.CLASS_CHECKED);
-                    attributes.label.removeClass(this.CLASS_CHECKED);
-                    if(attributes.wrapper) {
-                        attributes.wrapper.removeClass(this.CLASS_CHECKED);
-                    }
+                    //prevent infinite loops
+                    attributes.ignoreChangeEvent = true;
+                    masterNode.trigger("change");
+                    masterNode.trigger(this.EVENT_CHANGE, [this, checked]);
                 }
             }
         },
-        _updateState : function(state,cssClass){
+        _updateState          : function (state, cssClass) {
             var attributes = this.attributes;
-            if(attributes.disabled === false || attributes.masterNode.attr("disabled") === undefined) {
+            if (attributes.disabled === false || attributes.masterNode.attr("disabled") === undefined) {
                 if (state === true) {
                     attributes.falselyInput.addClass(cssClass);
                     attributes.label.addClass(cssClass);
@@ -166,96 +143,112 @@ var CustomCR = (function () {
                 }
             }
         },
-        _updateHoverState : function(isHover){
-            this._updateState(isHover,this.CLASS_HOVER);
+        _updateHoverState     : function (isHover) {
+            this._updateState(isHover, this.CLASS_HOVER);
         },
-        _updateActiveState : function(isActive){
-            this._updateState(isActive,this.CLASS_ACTIVE);
+        _updateActiveState    : function (isActive) {
+            this._updateState(isActive, this.CLASS_ACTIVE);
         },
-        _updateFocusState : function(hasFocus){
+        _updateFocusState     : function (hasFocus) {
             this._updateState(hasFocus, this.CLASS_FOCUS);
         },
-        _onMouseOver: function () {
-            if(this.attributes.touchEnd === false) {
+        _onMouseOver          : function () {
+            if (this.attributes.touchEnd === false) {
                 this._updateHoverState(true);
-            }else{
+            } else {
                 this.attributes.touchEnd = false;
             }
         },
-        _onKeyDown: function (keyCode) {
+        _onKeyDown            : function (keyCode) {
             //control too much events
-            if(this.attributes.keyDown !== true) {
+            if (this.attributes.keyDown !== true) {
                 this.attributes.keyDown = true;
                 if (keyCode === 13 || keyCode === 32) {
                     this._updateActiveState(true);
                 }
             }
         },
-        _onKeyUp: function (keyCode) {
+        _onKeyUp              : function (keyCode) {
             this.attributes.keyDown = false;
-            if(keyCode === 13 || keyCode === 32){
+            if (keyCode === 13 || keyCode === 32) {
                 this._updateActiveState(false);
             }
         },
-        _onEventTriggered : function(e){
+        _onEventTriggered     : function (e) {
             var data = e.data,
                 instance = data.instance,
                 type = e.type;
-            switch(type){
+            switch (type) {
                 case "click":
-                    console.log("click",e);
+                    console.log("click", e);
+                    var target = e.target,
+                        attributes = instance.attributes;
+                    if (target === attributes.falselyInput.get(0)) {
+                        instance._setChecked(!attributes.masterNode.prop("checked"));
+                    } else if (target === attributes.masterNode.get(0)) {
+                        /*by default, the native input is inside of the custom input, if the native input has position and opacity and it's clicked,
+                         *the click event is propagated up to the custom input and cause double event. In order to avoid this, when the native input trigger a click event,
+                         *the propagation is canceled.
+                         *This also control the propagation when native input is child of label (when label is clicked the native input trigger change)*/
+                        e.stopPropagation();
+                    }
                     break;
                 case "touchstart":
-                    console.log("touchstart",e);
+                    console.log("touchstart", e);
                     instance._updateActiveState(true);
                     break;
                 case "touchend":
-                    console.log("touchend",e);
+                    console.log("touchend", e);
                     instance._updateActiveState(false);
                     //control ghost hover in touch screens
                     instance._onTouchEnd();
                     break;
                 case "mousedown":
-                    console.log("mousedown",e);
+                    console.log("mousedown", e);
                     instance._updateActiveState(true);
                     break;
                 case "mouseup":
-                    console.log("mouseup",e);
+                    console.log("mouseup", e);
                     instance._updateActiveState(false);
                     break;
                 case "mouseover":
-                    console.log("mouseover",e);
+                    console.log("mouseover", e);
                     instance._onMouseOver();
                     break;
                 case "mouseout":
-                    console.log("mouseout",e);
+                    console.log("mouseout", e);
                     instance._updateHoverState(false);
                     break;
                 case "change":
-                    console.log("change",e);
+                    if (instance.attributes.ignoreChangeEvent !== true) {
+                        console.log("change", e);
+                        instance._setChecked(this.checked);
+                    } else {
+                        instance.attributes.ignoreChangeEvent = false;
+                    }
                     break;
                 case "focus":
-                    console.log("focus",e);
+                    console.log("focus", e);
                     instance._updateFocusState(true);
                     break;
                 case "blur":
-                    console.log("blur",e);
+                    console.log("blur", e);
                     instance._updateFocusState(false);
                     break;
                 case "keydown":
-                    console.log("keydown",e);
+                    console.log("keydown", e);
                     instance._onKeyDown(e.keyCode);
                     break;
                 case "keyup":
-                    console.log("keyup",e);
+                    console.log("keyup", e);
                     instance._onKeyUp(e.keyCode);
                     break;
             }
         },
-        _onTouchEnd : function(){
+        _onTouchEnd           : function () {
             this.attributes.touchEnd = true;
         },
-        _onInputChange: function (e) {
+        _onInputChange        : function (e) {
         },
         _onFalselyInputOperated: function (e) {
         }
